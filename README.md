919415f284950a5d2ffc0323dbc7a437d132985c
itzcookiie <mayowaadeniyi@hotmail.com>
2022-06-14 16:35:05 +0100

    feat: Add the parallax section, opacity fade in and refactor
    Make the scroll work with percentage instead of aggregating
    the scroll based on distance - very easy to bug out. But
    working with percentages is perfect since we know how far in
    the animation it should be based on the scroll position/height.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mac-page-w-parallax
+
+
+### Idea
+
+#### How to implement scrolling animating text
+- Place the first text in the middle and hide everything else
+- Set trigger points 2 scrolls apart between each text (use *wheelScrollDistance* for calculating that)
+- Create an animation function where it takes 3 params, the 2 elements in question and the direction to animate
+- Idea here is start with 2 elements (the first two) and try animating after 2 scrolls
+- Also need to add opacity scroll animation to first text - make it appear more gradual as you get closer to the parallax section vh
--- index.js
+++ index.js
@@ -1,40 +1,58 @@
+const wheelScrollDistance = 100,
+    heroSection = document.querySelector('.hero');
+
 const heroText = {
-    element: document.querySelector('.hero__text'),
-    scale: 28,
-    change: function(distance) {
-        return (8 * distance) / innerHeight
+        element: document.querySelector('.hero__text'),
+        initialScale: 28,
+        change: function() {
+            return this.initialScale - (8 * (scrollY / innerHeight));
+        },
+        transform: function() {
+            if (this.initialScale <= 25) return;
+            this.element.style.transform = `translate(-50%, -50%) scale(${this.change()})`
+        }
     },
-    increase: function(distance) {
-        this.scale += this.change(distance)
+    heroImg = {
+        element: document.querySelector('.hero__img'),
+        initialScale: 1.2,
+        change: function() {
+            return this.initialScale + (0.2 * (scrollY / innerHeight));
+        },
+        transform: function() {
+            if (this.initialScale >= 1.25) return;
+            this.element.style.transform = `translate(-50%, -50%) scale(${this.change()})`
+        }
     },
-    transform: function() {
-        if (this.scale <= 25) return;
-        this.element.style.transform = `translate(-50%, -50%) scale(${this.scale})`
-    }
-}, heroImg = {
-    element: document.querySelector('.hero__img'),
-    scale: 1.2,
-    change: function(distance) {
-        return (0.2 * distance) / innerHeight
+    parallaxSection = {
+        element: document.querySelector('.parallax'),
+        height: wheelScrollDistance * 7,
+        startPoint: heroSection.offsetHeight,
+        initialBrightness: 0,
+        endPoint: function() {
+            return this.element.offsetHeight;
+        },
+        change: function() {
+            return this.initialBrightness + 1 * (scrollY / this.startPoint)
+        },
+        transform: function() {
+            this.element.style.filter = `brightness(${this.change()})`
+        }
     },
-    increase: function(distance) {
-        this.scale += this.change(distance)
-    },
-    transform: function() {
-        if (this.scale >= 1.25) return;
-        this.element.style.transform = `translate(-50%, -50%) scale(${this.scale})`
-    }
-};
+    parallaxTextList = {
+        element: document.querySelector('.parallax__text-list'),
+        elements: function() {
+            return this.element.children;
+        }
+    };
 
 let lastScrollY = 0;
 
 window.addEventListener('scroll', e => {
-    if (scrollY >= innerHeight) return;
-    const distanceTravelled = scrollY - lastScrollY;
-    heroText.increase(-distanceTravelled);
-    heroText.transform();
+    if (scrollY <= heroSection.offsetHeight) {
+        heroText.transform();
+        heroImg.transform();
+        parallaxSection.transform();
+    }
 
-    heroImg.increase(distanceTravelled);
-    heroImg.transform();
     lastScrollY = scrollY;
 })
