598c70e62ba2d9e33dd464c621beedd4e69c9e97
itzcookiie <mayowaadeniyi@hotmail.com>
2022-06-15 16:39:10 +0100

    feat: Finished project MVP (or like 90%)
    Only thing left is to add the extra up and down
    transitions from moving the text elements up/down
    more when they are leaving.
    
    Gonna choose to leave it as something ik how to do
    in my head kinda thing (don't think i'll work on this
    anymore).
    
    Not until I watch the vid on how to scroll animate a
    video.

--- README.md
+++ README.md
@@ -9,3 +9,37 @@
 - Create an animation function where it takes 3 params, the 2 elements in question and the direction to animate
 - Idea here is start with 2 elements (the first two) and try animating after 2 scrolls
 - Also need to add opacity scroll animation to first text - make it appear more gradual as you get closer to the parallax section vh
+
+
+## Parallax
+
+#### Working remaining
+- Add logic to check if we've crossed any boundaries
+- If we have then add the opacity animation to the particular elements
+- If not then move the current element in the scroll direction
+
+
+#### How the OG Mac page works
+- Text are put inside *<ul>* container element as <li>s
+- Container is set in center of screen by making the top padding 50vh
+- Last text is positioned further down from the center (like most of the elements) as the padding bottom is set to 25vh (so 1/4 of viewport)
+- Text is spaced out by using padding
+- Text is faded in and out using transition opacity
+- They have some kinda algorithm that tells them when one text is in view or in the center
+- When the right text is detected, the index is added to the <ul> container
+- E.g. if it's the first text, the container will have a class of show-0, if it's the 2nd, show-1 etc
+- They use this to determine what class to add to the text elements
+- Each text element has a different class depending on what text is currently showing (or the scroll position)
+- E.g. when the parent container has a class of show-0, each text element will have CSS based on that, when it's show-1, each element will have CSS based on that etc
+- This is done by targeting the text elements through the parent class 
+- e.g. if the ul container has a class of ul-parent, targetting the 2nd element when the first is showing would look something like e.g. document.querySelector('.ul-parent.show-0:nth-child(2))
+- So each text element will have 4 different CSS states (technically only 3 since it's either not showing and positioned up, showing or not showing positioned down)
+
+#### Learnings
+- Pretty smart how they centered the container in the screen using top/bottom padding
+- Also sorts out starting/ending position compared to using *position:absolute* where you have to constantly keep track of everything
+- There's this cool thing in JS called *IntersectionObserver* which seems to do most of what we want
+- Prob try learn what it is/does at a high level. Won't get too into it since we are mostly done with this as we know how it works now
+- 
+
+[comment]: <> (- Also need to add conditons to make sure scrolling starts at a certain height &#40;and doesn't go beyond that&#41;)
--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const wheelScrollDistance = 100,
+    numScrolls = 6,
     heroSection = document.querySelector('.hero');
 
 const heroText = {
         element: document.querySelector('.hero__text'),
         initialScale: 28,
         changeScaleBy: 8,
+        maxScale: 20,
         change: function(scrollDistance) {
             return this.initialScale - (this.changeScaleBy * (scrollDistance / innerHeight));
         },
         transform: function(scrollDistance) {
-            this.element.style.transform = `translate(-50%, -50%) scale(${this.change(scrollDistance)})`
+            const changeScrollDistance = this.change(scrollDistance);
+            const scale = changeScrollDistance <= this.maxScale ? this.maxScale : changeScrollDistance;
+            this.element.style.transform = `translate(-50%, -50%) scale(${scale})`
         }
     },
     heroImg = {
         element: document.querySelector('.hero__img'),
         initialScale: 1.2,
         changeScaleBy: 0.2,
+        maxScale: 1.3,
         change: function(scrollDistance) {
             return this.initialScale + (this.changeScaleBy * (scrollDistance / innerHeight));
         },
         transform: function(scrollDistance) {
-            this.element.style.transform = `translate(-50%, -50%) scale(${this.change(scrollDistance)})`
+            const changeScrollDistance = this.change(scrollDistance);
+            const scale = changeScrollDistance >= this.maxScale ? this.maxScale : changeScrollDistance;
+            this.element.style.transform = `translate(-50%, -50%) scale(${scale})`
         }
     },
     parallaxSection = {
         element: document.querySelector('.parallax'),
-        height: wheelScrollDistance * 7,
+        height: wheelScrollDistance * 6,
         startPoint: heroSection.offsetHeight,
         initialBrightness: 0,
         endPoint: function() {
@@ -40,8 +47,60 @@ const heroText = {
     },
     parallaxTextList = {
         element: document.querySelector('.parallax__text-list'),
+        boundaries: [0, 1, 2, 3],
+        lastBoundary: null,
         elements: function() {
-            return this.element.children;
+            return [...this.element.children];
+        },
+        findCurrentBoundary: function() {
+            const [firstTrigger, secondTrigger, thirdTrigger] = this.triggerPoints();
+            if (scrollY < firstTrigger) return this.boundaries[0]
+            if (scrollY >= firstTrigger && scrollY < secondTrigger) return this.boundaries[1]
+            if (scrollY >= secondTrigger && scrollY < thirdTrigger) return this.boundaries[2]
+            if (scrollY >= thirdTrigger) return this.boundaries[3]
+        },
+        currentElement: function() {
+            return this.elements()[this.lastBoundary];
+        },
+        currentElementIndex: function() {
+            return this.elements().findIndex(element => element === this.currentElement());
+        },
+        prevElement: function() {
+            const currentElementIndex = this.currentElementIndex();
+            if (currentElementIndex === 0) return null;
+            return this.elements()[currentElementIndex - 1];
+        },
+        nextElement: function() {
+            const currentElementIndex = this.currentElementIndex();
+            if (currentElementIndex === this.elements().length - 1) return null;
+            return this.elements()[currentElementIndex + 1];
+        },
+        moveElements: function() {
+          this.elements().forEach((element, i) =>
+              element.style.top = `calc(50% - ${i * -200}px - ${scrollY - heroSection.offsetHeight}px)`
+          )
+        },
+        hide: function(element) {
+            element.style.opacity = '0';
+        },
+        show: function(element) {
+            element.style.opacity = '1';
+        },
+        triggerPoints: function() {
+            return Array(this.elements().length - 1).fill(0).map(
+                (n,i) => n + heroSection.offsetHeight + (wheelScrollDistance * 2 * (i+1))
+            )
+        },
+        haveCrossedTriggerPoint: function() {
+            return this.findCurrentBoundary() !== this.getLastBoundary();
+
+        },
+        getLastBoundary: function() {
+            return typeof (this.lastBoundary) === 'number' ? this.lastBoundary : this.findCurrentBoundary();
+        },
+        animate: function(element1, element2) {
+            this.hide(element1);
+            this.show(element2);
         }
     };
 
@@ -56,7 +115,17 @@ window.addEventListener('scroll', e => {
         heroText.transform(heroSection.offsetHeight);
         heroImg.transform(heroSection.offsetHeight);
         parallaxSection.transform(heroSection.offsetHeight);
+        const currElement = parallaxTextList.currentElement();
+        const scrollDiff = scrollY - lastScrollY;
+        if (parallaxTextList.haveCrossedTriggerPoint()) {
+            scrollDiff > 0 // Going up
+            ? parallaxTextList.animate(currElement, parallaxTextList.nextElement())
+            : parallaxTextList.animate(currElement, parallaxTextList.prevElement());
+        }
+        parallaxTextList.moveElements();
     }
 
     lastScrollY = scrollY;
+    parallaxTextList.lastBoundary = parallaxTextList.findCurrentBoundary();
 })
+
